2905995b1bd6c0912fd39f56d52c345b9193b579
Huakun Shen <huakun.shen@huakunshen.com>
2023-12-06 06:41:41 -0500

    Clean env variable

--- modules/utils.py
+++ modules/utils.py
@@ -9,10 +9,8 @@ def compute_sha512(input_string):
     return sha512_hash.hexdigest()
 
 def get_supabase() -> Client:
-    # supabase_url = os.environ.get("SUPABASE_URL")
-    supabase_url = "https://utcfesekjcfcqqauqxsb.supabase.co"
-    # supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
-    supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Y2Zlc2VramNmY3FxYXVxeHNiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTc2MjU3MiwiZXhwIjoyMDE3MzM4NTcyfQ.KtZU09tZQR1PJsxMw2ppydg927TB2GmolOr_XeS-jDY"
+    supabase_url = os.environ.get("SUPABASE_URL")
+    supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
     supabase: Client = create_client(supabase_url, supabase_key)
     return supabase
 
